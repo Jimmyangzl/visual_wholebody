7e46e163f8ce62ed78294430be44045a076b7d42
Zhelin Yang <jimmy.yang@tum.de>
2025-04-15 17:07:59 +0800

    change num_envs

--- low-level/legged_gym/envs/manip_loco/b2z1_config.py
+++ low-level/legged_gym/envs/manip_loco/b2z1_config.py
@@ -111,7 +111,7 @@ class B2Z1RoughCfg( LeggedRobotCfg ):
         clip_actions = 100.
 
     class env:
-        num_envs = 1
+        num_envs = 1000
         num_actions = 12 + 6 #CAUTION
         num_torques = 12 + 6
         action_delay = 3  # -1 for no delay
